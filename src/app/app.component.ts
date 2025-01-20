be4cc0bc4458130ef007af3e50088a0d921d0ba8
menepp <166714257+menepp@users.noreply.github.com>
2025-01-20 12:14:06 +0100

    menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { EmployeesComponent } from './components/employees/employees.component';
 import { ButtonComponent } from './components/button/button.component';
+import { MenuComponent } from './menu/menu.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [SearchBarComponent, EmployeesComponent, ButtonComponent],
+  imports: [SearchBarComponent, EmployeesComponent, ButtonComponent, MenuComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+  menuOpen: boolean = false;
 
+  toggleMenu(): void {
+    this.menuOpen = !this.menuOpen; // Inverse l'état du menu
+  }
 }
