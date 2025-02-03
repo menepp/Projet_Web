9175f244a0fb751fa914a381d8ebe849c5290f59
Axel-mlv <malavalaxel@gmail.com>
2025-02-03 13:51:01 +0100

    CSS de mission

--- src/app/mission/mission.component.ts
+++ src/app/mission/mission.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CarteMissionComponent } from './carte-mission/carte-mission.component';
 import { Mission } from '../models/mission.interface';
+import { SearchBarComponent } from "../components/search-bar/search-bar.component";
 
 @Component({
   selector: 'app-mission',
-  imports: [CarteMissionComponent],
+  imports: [CarteMissionComponent, SearchBarComponent],
 
   templateUrl: './mission.component.html',
   styleUrl: './mission.component.css'
@@ -14,17 +15,21 @@ export class MissionComponent implements OnInit{
   missions: Mission[] = [];
   isLoading = true;
 
-  ngOnInit(): void{
+  ngOnInit(): void {
+    this.fetchMissions();
+  }
+
+  fetchMissions(){
     fetch('http://localhost:3000/api/missions')
     .then((response) => response.json())
       .then((data) => {
-        this.missions = data; // Stocker les données dans le tableau
-        this.isLoading = false; // Masquer l'indicateur de chargement
+        this.missions = data; 
+        this.isLoading = false; 
         console.log(this.missions);
       })
       .catch((error) => {
         console.error('Erreur lors de la récupération des missions:', error);
-        this.isLoading = false; // Masquer l'indicateur même en cas d'erreur
+        this.isLoading = false; 
       });
   }
 }
