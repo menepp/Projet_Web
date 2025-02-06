6e3d526a50e48faf8668e4cf67951e7fe5f577b9
aliciajaber <aliciamjaber@gmail.com>
2025-02-06 18:07:16 +0100

    Header + Maj Menu + import images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import {RouterOutlet} from '@angular/router';
 import {MenuComponent} from './menu/menu.component';
 import {AideComponent} from './aide/aide.component';
 import {FooterComponent} from './footer/footer.component';
+import {HeaderComponent} from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MenuComponent, AideComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, MenuComponent, AideComponent, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']  // Notez le "styleUrls" (avec "s")
 })
